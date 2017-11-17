45c6d8ef20996d73aa01f18e975e36b656f4e1e9
AngelRisso1337z <angel.risso@unitec.edu>
2017-11-17 13:40:02 -0600

    creando clases y gui

---
+++ lab5/src/lab5/Equipos.java
@@ -0,0 +1,11 @@
+package lab5;
+
+import java.util.ArrayList;
+
+public class Equipos {
+    private String nombre_deEquipo;
+    private int copas;
+    private double presupuesto;
+    private ArrayList<Jugador> jugadores=new ArrayList();
+    private String Estadio;
+}
---
+++ lab5/src/lab5/Jugador.java
@@ -0,0 +1,86 @@
+package lab5;
+
+
+public class Jugador {
+    private String nombre;
+    private double habilidad;
+    private double tecnica;
+    private double resistencia;
+    private double precio;
+    private boolean dispon;
+    private String pocicion;
+
+    public Jugador() {
+    }
+
+    public Jugador(String nombre, double habilidad, double tecnica, double resistencia, double precio, boolean dispon) {
+        this.nombre = nombre;
+        this.habilidad = habilidad;
+        this.tecnica = tecnica;
+        this.resistencia = resistencia;
+        this.precio = precio;
+        this.dispon = dispon;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(double habilidad) {
+        this.habilidad = habilidad;
+    }
+
+    public double getTecnica() {
+        return tecnica;
+    }
+
+    public void setTecnica(double tecnica) {
+        this.tecnica = tecnica;
+    }
+
+    public double getResistencia() {
+        return resistencia;
+    }
+
+    public void setResistencia(double resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDispon() {
+        return dispon;
+    }
+
+    public void setDispon(boolean dispon) {
+        this.dispon = dispon;
+    }
+
+    public String getPocicion() {
+        return pocicion;
+    }
+
+    public void setPocicion(String pocicion) {
+        this.pocicion = pocicion;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre + ", precio=" + precio + ", pocicion=" + pocicion + '}';
+    }
+    
+}
